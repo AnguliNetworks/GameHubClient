9a0c1883a4cee041dedc58faf82f0f1ca5191463
Fin Reinhard <fin.reinhard@icloud.com>
2018-02-09 16:24:32 +0100

    added status check before launching

--- src/App.jsx
+++ src/App.jsx
@@ -22,36 +22,53 @@ class App extends Component {
     }
 
     loadUI() {
-        if (this.cookies.get('session') && this.cookies.get('userId')) {
-            // TODO CHANGE URL
-            fetch('http://localhost:8080/session-check', {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                },
-                body: 'userId=' + this.cookies.get('userId') + '&session=' + this.cookies.get('session')
-            })
-                .then((body) => body.json())
-                .then((json) => {
-                    this.setState({loggedIn: json.status === 'ACCEPTED'});
-                    if (json.status !== 'ACCEPTED') {
-                        return;
+        fetch('http://localhost:8080/status', {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/x-www-form-urlencoded',
+            }
+        })
+            .then(body => body.json())
+            .then(json => {
+                if (json.status === 'OK') {
+                    if (this.cookies.get('session') && this.cookies.get('userId')) {
+                        // TODO CHANGE URL
+                        fetch('http://localhost:8080/session-check', {
+                            method: 'POST',
+                            headers: {
+                                'Accept': 'application/json',
+                                'Content-Type': 'application/x-www-form-urlencoded',
+                            },
+                            body: 'userId=' + this.cookies.get('userId') + '&session=' + this.cookies.get('session')
+                        })
+                            .then((body) => body.json())
+                            .then((json) => {
+                                this.setState({loggedIn: json.status === 'ACCEPTED'});
+                                if (json.status !== 'ACCEPTED') {
+                                    return;
+                                }
+
+                                this.setState({loading: true});
+
+                                // TODO FURTHER UI LOADING
+                            });
+                    } else {
+                        this.setState({loggedIn: false});
                     }
-
-                    this.setState({loading: true});
-
-                    // TODO FURTHER UI LOADING
-                });
-        } else {
-            this.setState({loggedIn: false});
-        }
+                } else {
+                    this.setState({offline: true});
+                }
+            })
+            .catch(() => {
+                this.setState({offline: true});
+            });
     }
 
     render() {
         if (this.state.loggedIn === undefined || this.state.loading) {
             return (
-                <LoadingScreen/>
+                <LoadingScreen offline={this.state.offline}/>
             );
         }
         let login = this.state.loggedIn ? <div/> : <Login afterLogin={this.loadUI}/>;
--- src/components/views/LoadingScreen.jsx
+++ src/components/views/LoadingScreen.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
 class LoadingScreen extends React.Component {
+
     render() {
+        if (this.props.offline) {
+            return (
+                <div className={'loading-screen offline'}>
+                    <h1>Huch, hier stimmt was nicht :o</h1>
+                    {
+                        navigator.onLine ?
+                            <span>
+                                Unsere Server scheinen gerade Offline zu sein :/ Mehr Infos findest Du auf
+                                <a href={'http://twitter.com/GameHubOne'}> Twitter</a> oder auf
+                                <a href={'https://status.gamehub.one'}> unserer Status-Seite</a>.
+                            </span> :
+                            <span>
+                                Du bist gerade nicht mit dem Internet verbunden. OfflinePlay wird leider
+                                <i> noch</i> nicht unterstützt :/
+                            </span>
+                    }
+                </div>
+            );
+        }
+
         return (
-            <div className={'loading'}>
+            <div className={'loading-screen loading'}>
                 <div className={'spinner'}>
                     <div className={'bounce double-bounce1'}/>
                     <div className={'bounce double-bounce2'}/>
