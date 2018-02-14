d66f7c603de3fa04579ba6a42628a4f70355a9f1
Fin Reinhard <fin.reinhard@icloud.com>
2018-02-15 00:40:45 +0100

    added eslint
    changed example pictures

--- src/components/views/launcher/View.jsx
+++ src/components/views/launcher/View.jsx
@@ -16,8 +16,8 @@ class Launcher extends React.Component {
                     <Column size={'small'}>
                         <Panel id={'profile'} transparent>
                             <UserInfo
-                                avatar={'https://picsum.photos/64/64/'}
-                                username={'Gronkh'}
+                                avatar={'https://pbs.twimg.com/profile_images/932304084253671425/HuOBI7ix_400x400.jpg'}
+                                username={'Fin'}
                                 status={'Online'}
                             />
                             <ul className={'inline-menu'}>
@@ -40,36 +40,30 @@ class Launcher extends React.Component {
                             <div className={'friend-list'}>
                                 <Avatar
                                     status={'ingame'}
-                                    src={'https://picsum.photos/64/64/'}
+                                    src={'https://pbs.twimg.com/profile_images/961720742395219968/T80-pxAo_400x400.jpg'}
                                 />
                                 <Avatar
                                     status={'ingame'}
-                                    src={'https://picsum.photos/64/64/'}
+                                    src={'https://pbs.twimg.com/profile_images/963123405246562304/Ucf2P3mD_400x400.jpg'}
                                 />
                                 <Avatar
                                     status={'ingame'}
-                                    src={'https://picsum.photos/64/64/'}
+                                    src={'https://pbs.twimg.com/profile_images/584328543313723392/wFxDEHV3_400x400.jpg'}
                                 />
                                 <Avatar
                                     status={'online'}
-                                    src={'https://picsum.photos/64/64/'}
+                                    src={'https://pbs.twimg.com/profile_images/923193829666238464/wbdjH5OI_400x400.jpg'}
                                 />
                                 <Avatar
                                     status={'busy'}
-                                    src={'https://picsum.photos/64/64/'}
+                                    src={'https://pbs.twimg.com/profile_images/878973681489281025/tzjdE7f1_400x400.jpg'}
                                 />
                                 <Avatar
                                     status={'afk'}
-                                    src={'https://picsum.photos/64/64/'}
+                                    src={'https://pbs.twimg.com/profile_images/880108605659385857/w3elReLI_400x400.jpg'}
                                 />
                                 <Avatar
-                                    src={'https://picsum.photos/64/64/'}
-                                />
-                                <Avatar
-                                    src={'https://picsum.photos/64/64/'}
-                                />
-                                <Avatar
-                                    src={'https://picsum.photos/64/64/'}
+                                    src={'https://pbs.twimg.com/profile_images/949657515624714241/kOL2_Heq_400x400.jpg'}
                                 />
                             </div>
                         </Panel>
@@ -81,13 +75,13 @@ class Launcher extends React.Component {
                             </h1>
                             <div className={'game-list'}>
                                 <GameOverview
-                                    icon={'https://picsum.photos/64/64/'}
-                                    name={'Counter-Strike: Global Offensive'}
+                                    icon={'https://img.etsystatic.com/il/6b3848/1028207441/il_570xN.1028207441_og72.jpg'}
+                                    name={'TBA'}
                                     status={'zuletzt gespielt: heute'}
                                 />
                                 <GameOverview
-                                    icon={'https://picsum.photos/64/64/'}
-                                    name={'Counter-Strike: Global Offensive'}
+                                    icon={'https://img.etsystatic.com/il/6b3848/1028207441/il_570xN.1028207441_og72.jpg'}
+                                    name={'TBA'}
                                     status={'zuletzt gespielt: heute'}
                                 />
                             </div>
